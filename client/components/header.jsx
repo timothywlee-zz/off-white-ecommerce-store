63a05cafc5c01dd39a2e21b7b7a75090dd4f32b8
Timothy Lee <timothylee3508@gmail.com>
2020-01-13 17:29:24 -0800

    changed css styling to inline-styling instead of using external stylesheet

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Header(props) {
   return (
-    <div className='header text-light bg-dark col-12 w-100 m-0 '>
+    <div className='text-light bg-dark col-12 w-100 m-0' style={{ height: '5vh', padding: '5px 0' }}>
       <div className='col-12 pl-5'>${props.title}</div>
     </div>
   );
