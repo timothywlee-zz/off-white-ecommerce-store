bae7d77f45f564689bd692088ae427f57569cd95
timmmleee <timothylee3508@gmail.com>
2020-01-15 23:21:50 +0000

    added a feature where the user can look at all the items in their shopping cart

---
+++ client/components/cart-summary-item.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function CartSummaryItem(props) {
+  return (
+    <div className='container border border-dark m-3 shadow-sm'>
+      <div className='row'>
+        <img src={props.product.image} className='picture' style={{ objectFit: 'contain', height: '35vh' }}/>
+        <div className='d-flex flex-column py-5 mx-4'>
+          <h4> {props.product.name} </h4>
+          <div className='text-muted'> ${(props.product.price / 100).toFixed(2)}  </div>
+          <div className='my-2' style={{ fontSize: '0.9rem' }}> {props.product.shortDescription} </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CartSummaryItem;
--- client/components/header.jsx
+++ client/components/header.jsx
@@ -7,7 +7,7 @@ function Header(props) {
     <div className='text-light bg-dark col-12 m-0' style={{ height: '7vh', padding: '1.7vh 9vh' }}>
       <div className='d-flex justify-content-between'>
         <div className=''>$ {props.title}</div>
-        <div className='d-flex flex-row'>
+        <div className='d-flex flex-row' onClick={() => props.setView('cart', {})} style={{ cursor: 'pointer' }} >
           <div className=' mx-1'>{props.cartItemCount}</div>
           <div className='mr-2'> {itemsInCartCount} </div>
           <i className="fas fa-shopping-cart" style={{ fontSize: '1.5rem' }}></i>
