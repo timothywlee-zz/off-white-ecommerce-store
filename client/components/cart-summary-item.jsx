78db717a9d7ceabe5bf0c674cc89c19e2a2ed86a
timothywlee <timothylee3508@gmail.com>
2020-02-04 03:11:27 +0000

    removed short description from cart summary page

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -11,9 +11,6 @@ function CartSummaryItem(props) {
         <div className='d-flex flex-column py-5 mx-4'>
           <h4> {props.product.name} </h4>
           <div className='text-muted'> ${(props.product.price / 100).toFixed(2)}  </div>
-          <div
-            className='my-2'
-            style={{ fontSize: '0.9rem' }}> {props.product.shortDescription} </div>
         </div>
         <div className='d-flex flex-column justify-content-center align-items-center' style={{ width: '14%' }}>
           <div>Quantity</div>
